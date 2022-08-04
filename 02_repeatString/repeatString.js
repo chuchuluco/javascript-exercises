fad40a0ac91ef9c59fdb5171bf972441076ee437
chuchuluco <oscarfdez05@gmail.com>
2022-08-04 14:14:11 -0500

    Complete first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let repeatedStr = "";
+  if (num < 0) {
+    return "ERROR"
+  } else {
+    for (i = 0; i < num; i++) {
+      repeatedStr += `${str}`
+    }
+  }
+  return repeatedStr
 };
 
 // Do not edit below this line
